888325d2d259cc7dd4860da00823c93c0a6ddd1b
serhack <atombt@protonmail.com>
2017-06-27 14:55:02 +0200

    Update gateway.php

--- monero/gateway.php
+++ monero/gateway.php
@@ -1,1 +1,7 @@
 <?php
+/* Main Gateway of Monero using a daemon online */
+class Monero_Gateway extends WC_Payment_Gateway {
+    function __construct(){
+      	$this->id = "monero_gateway";
+		    $this->method_title = __("Monero GateWay", 'monero_gateway');
+}
