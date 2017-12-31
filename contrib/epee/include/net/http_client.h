dfe4a4d1eabe842c68c2ca174ab515f0655aaede
SadBatman <niels.heinen@gmail.com>
2017-12-31 23:52:27 +0100

    Add SSL support to the http client / net helper.

--- contrib/epee/include/net/http_client.h
+++ contrib/epee/include/net/http_client.h
@@ -1,6 +1,6 @@
 // Copyright (c) 2006-2013, Andrey N. Sabelnikov, www.sabelnikov.net
 // All rights reserved.
-// 
+//
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are met:
 // * Redistributions of source code must retain the above copyright
@@ -11,7 +11,7 @@
 // * Neither the name of the Andrey N. Sabelnikov nor the
 // names of its contributors may be used to endorse or promote products
 // derived from this software without specific prior written permission.
-// 
+//
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
 // ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 // WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -22,7 +22,7 @@
 // ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-// 
+//
 
 
 
@@ -40,11 +40,11 @@
 
 #ifdef HTTP_ENABLE_GZIP
 #include "gzip_encoding.h"
-#endif 
+#endif
 
 #include "string_tools.h"
 #include "reg_exp_definer.h"
-#include "http_base.h" 
+#include "http_base.h"
 #include "to_nonconst_iterator.h"
 #include "net_parse_helpers.h"
 
@@ -60,9 +60,9 @@ namespace epee
 namespace net_utils
 {
 
-using namespace std; 
+using namespace std;
 
-	/*struct url 
+	/*struct url
 	{
 	public:
 		void parse(const std::string& url_s)
@@ -125,8 +125,8 @@ using namespace std;
 
 		return false;
 	}
-	
-	static inline 
+
+	static inline
 		std::string dec_to_hex(char num, int radix)
 	{
 		int temp=0;
@@ -150,14 +150,14 @@ using namespace std;
 		{
 			csTmp += '0';
 		}
-		
+
 		std::reverse(csTmp.begin(), csTmp.end());
 		//_mbsrev((unsigned char*)csTmp.data());
 
 		return csTmp;
 	}
 	static inline int get_index(const char *s, char c) { const char *ptr = (const char*)memchr(s, c, 16); return ptr ? ptr-s : -1; }
-	static inline 
+	static inline
 		std::string hex_to_dec_2bytes(const char *s)
 	{
 		const char *hex = get_hex_vals();
@@ -234,7 +234,7 @@ using namespace std;
 		class http_simple_client: public i_target_handler
 		{
 		public:
-			
+
 
 		private:
 			enum reciev_machine_state
@@ -271,8 +271,28 @@ using namespace std;
 			chunked_state m_chunked_state;
 			std::string m_chunked_cache;
 			critical_section m_lock;
+      bool m_ssl;
 
 		public:
+			explicit http_simple_client() : i_target_handler()
+ 				, m_net_client()
+ 				, m_host_buff()
+ 				, m_port()
+ 				, m_header_cache()
+ 				, m_response_info()
+ 				, m_len_in_summary(0)
+ 				, m_len_in_remain(0)
+ 				, m_pcontent_encoding_handler(nullptr)
+ 				, m_state()
+				, m_chunked_state()
+  			, m_chunked_cache()
+  			, m_lock()
+  			, m_ssl(false) {}
+
+			void enable_ssl() {
+        m_ssl = true;
+      }
+
 			void set_host_name(const std::string& name)
 			{
 				CRITICAL_REGION_LOCAL(m_lock);
@@ -289,7 +309,7 @@ using namespace std;
         m_port = port;
         m_timeout = timeout;
 
-        return m_net_client.connect(host,  port, timeout, timeout);
+        return m_net_client.connect(host,  port, timeout, timeout, m_ssl);
       }
 			//---------------------------------------------------------------------------
 			bool disconnect()
@@ -312,7 +332,7 @@ using namespace std;
 				return true;
 			}
 			//---------------------------------------------------------------------------
-			inline 
+			inline
 				bool invoke_get(const std::string& uri, const std::string& body = std::string(), const http_response_info** ppresponse_info = NULL, const fields_list& additional_params = fields_list())
 			{
 					CRITICAL_REGION_LOCAL(m_lock);
@@ -334,7 +354,7 @@ using namespace std;
 				}
 				m_response_info.clear();
 				std::string req_buff = 	method + " ";
-				req_buff += uri + " HTTP/1.1\r\n" + 
+				req_buff += uri + " HTTP/1.1\r\n" +
 					"Host: "+ m_host_buff +"\r\n" +	"Content-Length: " + boost::lexical_cast<std::string>(body.size()) + "\r\n";
 
 
@@ -362,7 +382,7 @@ using namespace std;
 				CRITICAL_REGION_LOCAL(m_lock);
 				return invoke(uri, "POST", body, ppresponse_info, additional_params);
 			}
-		private: 
+		private:
 			//---------------------------------------------------------------------------
 			inline bool handle_reciev()
 			{
@@ -430,7 +450,7 @@ using namespace std;
 			inline
 				bool handle_header(std::string& recv_buff, bool& need_more_data)
 			{
- 
+
 				CRITICAL_REGION_LOCAL(m_lock);
         if(!recv_buff.size())
         {
@@ -531,7 +551,7 @@ using namespace std;
 							continue;
 						}
 						else if(*it == '\n')
-						{	
+						{
 							std::string chunk_head = buff.substr(0, offset);
 							if(!get_len_from_chunk_head(chunk_head, chunk_size))
 								return false;
@@ -561,7 +581,7 @@ using namespace std;
 
 							buff.erase(buff.begin(), ++it);
 
-							is_matched = true;				
+							is_matched = true;
 							return true;
 						}
 						else
@@ -664,13 +684,13 @@ using namespace std;
 			//---------------------------------------------------------------------------
 			inline
 				bool parse_header(http_header_info& body_info, const std::string& m_cache_to_process)
-			{ 
+			{
 				LOG_FRAME("http_stream_filter::parse_cached_header(*)", LOG_LEVEL_4);
-				
-				STATIC_REGEXP_EXPR_1(rexp_mach_field, 
+
+				STATIC_REGEXP_EXPR_1(rexp_mach_field,
 					"\n?((Connection)|(Referer)|(Content-Length)|(Content-Type)|(Transfer-Encoding)|(Content-Encoding)|(Host)|(Cookie)|(User-Agent)"
 					//  12            3         4                5              6                   7                  8      9        10
-					"|([\\w-]+?)) ?: ?((.*?)(\r?\n))[^\t ]",	
+					"|([\\w-]+?)) ?: ?((.*?)(\r?\n))[^\t ]",
 					//11             1213   14
 					boost::regex::icase | boost::regex::normal);
 
@@ -681,7 +701,7 @@ using namespace std;
 
 
 				//lookup all fields and fill well-known fields
-				while( boost::regex_search( it_current_bound, it_end_bound, result, rexp_mach_field, boost::match_default) && result[0].matched) 
+				while( boost::regex_search( it_current_bound, it_end_bound, result, rexp_mach_field, boost::match_default) && result[0].matched)
 				{
 					const size_t field_val = 13;
 					//const size_t field_etc_name = 11;
@@ -723,7 +743,7 @@ using namespace std;
 
 				//First line response, look like this:  "HTTP/1.1 200 OK"
 				STATIC_REGEXP_EXPR_1(rexp_match_first_response_line, "^HTTP/(\\d+).(\\d+) ((\\d)\\d{2})( [^\n]*)?\r?\n", boost::regex::icase | boost::regex::normal);
-				//															1      2      34           5        
+				//															1      2      34           5
 				//size_t match_len = 0;
 				boost::smatch result;
 				if(boost::regex_search( m_header_cache, result, rexp_match_first_response_line, boost::match_default) && result[0].matched)
@@ -732,7 +752,7 @@ using namespace std;
 					m_response_info.m_http_ver_hi   = boost::lexical_cast<int>(result[1]);
 					m_response_info.m_http_ver_lo   = boost::lexical_cast<int>(result[2]);
 					m_response_info.m_response_code = boost::lexical_cast<int>(result[3]);
-					
+
 					m_header_cache.erase(to_nonsonst_iterator(m_header_cache, result[0].first), to_nonsonst_iterator(m_header_cache, result[0].second));
 					return true;
 				}else
@@ -757,14 +777,14 @@ using namespace std;
           return false;
 #endif
 				}
-				else 
+				else
 				{
 					m_pcontent_encoding_handler.reset(new do_nothing_sub_handler(this));
 				}
 
 				return true;
 			}
-			inline	
+			inline
 				bool analize_cached_header_and_invoke_state()
 			{
 				m_response_info.clear();
@@ -783,8 +803,8 @@ using namespace std;
 
 
 
-				if(!m_len_in_summary && ((m_response_info.m_response_code>=100&&m_response_info.m_response_code<200) 
-					|| 204 == m_response_info.m_response_code 
+				if(!m_len_in_summary && ((m_response_info.m_response_code>=100&&m_response_info.m_response_code<200)
+					|| 204 == m_response_info.m_response_code
 					|| 304 == m_response_info.m_response_code) )
 				{//There will be no response body, server will display the local page with error
 					m_state = reciev_machine_state_done;
@@ -803,7 +823,7 @@ using namespace std;
 					return true;
 				}
 				else if(!m_response_info.m_header_info.m_content_length.empty())
-				{ 
+				{
 					//In the response header the length was specified
 					if(!content_len_valid)
 					{
@@ -835,10 +855,10 @@ using namespace std;
 					m_state = reciev_machine_state_error;
 					LOG_PRINT("Undefinded transfer type, consider http_body_transfer_connection_close method. header: " << m_header_cache, LOG_LEVEL_2);
 					return false;
-				} 
+				}
 				return false;
 			}
-			inline 
+			inline
 				bool is_connection_close_field(const std::string& str)
 			{
 				STATIC_REGEXP_EXPR_1(rexp_match_close, "^\\s*close", boost::regex::icase | boost::regex::normal);
@@ -862,7 +882,7 @@ using namespace std;
 						boundary = result[6];
 					else if(result[7].matched)
 						boundary = result[7];
-					else 
+					else
 					{
 						LOG_ERROR("Failed to match boundary in content-type=" << head_info.m_content_type);
 						return false;
@@ -881,7 +901,7 @@ using namespace std;
     /************************************************************************/
     /*                                                                      */
     /************************************************************************/
-  //inline 
+  //inline
     template<class t_transport>
     bool invoke_request(const std::string& url, t_transport& tr, unsigned int timeout, const http_response_info** ppresponse_info, const std::string& method = "GET", const std::string& body = std::string(), const fields_list& additional_params = fields_list())
     {
