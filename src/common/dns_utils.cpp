b6d8ca21febae0e7042d4697e796db80de564217
Sumo Gr <sumogr@mail.com>
2019-03-30 14:18:54 +0200

    MoneroPulse ---> SumoPulse

--- src/common/dns_utils.cpp
+++ src/common/dns_utils.cpp
@@ -516,7 +516,7 @@ bool load_txt_records_from_dns(std::vector<std::string> &good_records, const std
 
   if (num_valid_records < 2)
   {
-    LOG_PRINT_L0("WARNING: no two valid MoneroPulse DNS checkpoint records were received");
+    LOG_PRINT_L0("WARNING: no two valid SumoPulse DNS checkpoint records were received");
     return false;
   }
 
