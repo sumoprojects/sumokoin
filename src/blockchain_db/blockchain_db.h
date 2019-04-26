3a9d1071de4f69e3bc309716019e7b50df87e1c0
Sumo Gr <sumogr@mail.com>
2019-04-26 10:27:52 +0300

    Update blockchain_db.h

--- src/blockchain_db/blockchain_db.h
+++ src/blockchain_db/blockchain_db.h
@@ -1323,10 +1323,10 @@ public:
    * but for a list of outputs rather than just one.
    *
    * @param amount an output amount
-   * @param offsets a list of amount-specific output indices
+   * @param amounts an output amount, or as many as offsets
    * @param outputs return-by-reference a list of outputs' metadata
    */
-  virtual void get_output_key(const uint64_t &amount, const std::vector<uint64_t> &offsets, std::vector<output_data_t> &outputs, bool allow_partial = false) const = 0;
+  virtual void get_output_key(const epee::span<const uint64_t> &amounts, const std::vector<uint64_t> &offsets, std::vector<output_data_t> &outputs, bool allow_partial = false) = 0;
   
   /*
    * FIXME: Need to check with git blame and ask what this does to
