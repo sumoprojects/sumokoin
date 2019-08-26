1d0ef2ea5ef79b0a2f01a86609c56d7465199cb6
Sumo Gr <sumogr@mail.com>
2019-08-26 15:12:21 +0300

    Update syncing method

--- src/common/dns_utils.cpp
+++ src/common/dns_utils.cpp
@@ -564,7 +564,9 @@ bool load_txt_records_from_dns(std::vector<std::string> &good_records, const std
 
   if (num_valid_records < 2)
   {
-    LOG_PRINT_L0("WARNING: no two valid DNS TXT records were received");
+// suppress message
+//    LOG_PRINT_L0("WARNING: no two valid DNS TXT records were received");
+
     return false;
   }
 
