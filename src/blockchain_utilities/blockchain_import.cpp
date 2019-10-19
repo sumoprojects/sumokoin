8a0c456c7aeb07eadd1f666a312966e890822061
Aris-sumogr <54616978+Aris-sumogr@users.noreply.github.com>
2019-10-19 13:25:21 +0300

    [UTILITIES] Blockchain-import batch size
    batch_size= 100 was set for x32 archs while 20000 for x64 (this was due to a suspected db issue that couldnt handle large batches at 32 bit systems). 20000 means that sometimes even if you import from file you still have to sync normaly up to 20k blocks which is frustrating. Set batch size for all archs to 100. it will take a couple of minutes more to import the entire chain (-100 blocks worst case) than having to sync 20k blocks

--- src/blockchain_utilities/blockchain_import.cpp
+++ src/blockchain_utilities/blockchain_import.cpp
@@ -56,14 +56,7 @@ bool opt_resume  = true;
 bool opt_testnet = true;
 bool opt_stagenet = true;
 
-// number of blocks per batch transaction
-// adjustable through command-line argument according to available RAM
-#if ARCH_WIDTH != 32
-uint64_t db_batch_size = 20000;
-#else
-// set a lower default batch size, pending possible LMDB issue with large transaction size
 uint64_t db_batch_size = 100;
-#endif
 
 // when verifying, use a smaller default batch size so progress is more
 // frequently saved
