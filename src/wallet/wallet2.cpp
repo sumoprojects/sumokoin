8f40b332f34ebfa6a1b19bce29dbcb10a6dae98a
sumogr <sumogr@mail.com>
2020-02-06 21:56:53 +0000

    [wallet2] don't try to lock an empty filename

--- src/wallet/wallet2.cpp
+++ src/wallet/wallet2.cpp
@@ -7530,6 +7530,8 @@ bool wallet2::is_output_blackballed(const std::pair<uint64_t, uint64_t> &output)
 
 bool wallet2::lock_keys_file()
 {
+  if (m_wallet_file.empty())
+    return true;
   if (m_keys_file_locker)
   {
     MDEBUG(m_keys_file << " is already locked.");
@@ -7541,6 +7543,8 @@ bool wallet2::lock_keys_file()
 
 bool wallet2::unlock_keys_file()
 {
+  if (m_wallet_file.empty())
+    return true;
   if (!m_keys_file_locker)
   {
     MDEBUG(m_keys_file << " is already unlocked.");
@@ -7552,6 +7556,8 @@ bool wallet2::unlock_keys_file()
 
 bool wallet2::is_keys_file_locked() const
 {
+  if (m_wallet_file.empty())
+    return false;
   return m_keys_file_locker->locked();
 }
 
