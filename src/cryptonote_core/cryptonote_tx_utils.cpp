556a371655e2142c1945059be51203b497592e4a
sumogr <sumogr@mail.com>
2020-04-06 21:25:05 +0300

    code clean up

--- src/cryptonote_core/cryptonote_tx_utils.cpp
+++ src/cryptonote_core/cryptonote_tx_utils.cpp
@@ -104,7 +104,7 @@ namespace cryptonote
 #endif
     block_reward += fee;
 
-    crypto::key_derivation derivation = AUTO_VAL_INIT(derivation);;
+    crypto::key_derivation derivation = AUTO_VAL_INIT(derivation);
     crypto::public_key out_eph_public_key = AUTO_VAL_INIT(out_eph_public_key);
     bool r = crypto::generate_key_derivation(miner_address.m_view_public_key, txkey.sec, derivation);
     CHECK_AND_ASSERT_MES(r, false, "while creating outs: failed to generate_key_derivation(" << miner_address.m_view_public_key << ", " << txkey.sec << ")");
