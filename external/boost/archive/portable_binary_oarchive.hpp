826e816f3e6056207716e7eaf2b35e3616ccbe20
sumogr <sumogr@mail.com>
2020-04-09 20:41:31 +0300

    2

--- external/boost/archive/portable_binary_oarchive.hpp
+++ external/boost/archive/portable_binary_oarchive.hpp
@@ -14,7 +14,7 @@
 /////////1/////////2/////////3/////////4/////////5/////////6/////////7/////////8
 // portable_binary_oarchive.hpp
 
-// (C) Copyright 2002 Robert Ramey - http://www.rrsd.com . 
+// (C) Copyright 2002 Robert Ramey - http://www.rrsd.com .
 // Use, modification and distribution is subject to the Boost Software
 // License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
@@ -37,12 +37,12 @@ namespace boost { namespace archive {
 /////////1/////////2/////////3/////////4/////////5/////////6/////////7/////////8
 // exception to be thrown if integer read from archive doesn't fit
 // variable being loaded
-class portable_binary_oarchive_exception : 
+class portable_binary_oarchive_exception :
     public boost::archive::archive_exception
 {
 public:
     enum exception_code {
-        invalid_flags 
+        invalid_flags
     } m_exception_code ;
     portable_binary_oarchive_exception(exception_code c = invalid_flags ) :
         boost::archive::archive_exception(boost::archive::archive_exception::other_exception),
@@ -65,14 +65,14 @@ public:
 };
 
 /////////1/////////2/////////3/////////4/////////5/////////6/////////7/////////8
-// "Portable" output binary archive.  This is a variation of the native binary 
+// "Portable" output binary archive.  This is a variation of the native binary
 // archive. it addresses integer size and endienness so that binary archives can
 // be passed across systems. Note:floating point types not addressed here
 
 class portable_binary_oarchive :
     public boost::archive::basic_binary_oprimitive<
         portable_binary_oarchive,
-        std::ostream::char_type, 
+        std::ostream::char_type,
         std::ostream::traits_type
     >,
     public boost::archive::detail::common_oarchive<
@@ -81,7 +81,7 @@ class portable_binary_oarchive :
 {
     typedef boost::archive::basic_binary_oprimitive<
         portable_binary_oarchive,
-        std::ostream::char_type, 
+        std::ostream::char_type,
         std::ostream::traits_type
     > primitive_base_t;
     typedef boost::archive::detail::common_oarchive<
@@ -137,7 +137,7 @@ protected:
 
     // default processing - kick back to base class.  Note the
     // extra stuff to get it passed borland compilers
-    typedef boost::archive::detail::common_oarchive<portable_binary_oarchive> 
+    typedef boost::archive::detail::common_oarchive<portable_binary_oarchive>
         detail_common_oarchive;
 #if BOOST_VERSION > 105800
     template<class T>
@@ -149,7 +149,7 @@ protected:
         const std::string s(t);
         * this << s;
     }
-    // binary files don't include the optional information 
+    // binary files don't include the optional information
     void save_override(
         const boost::archive::class_id_optional_type & /* t */
     ){}
@@ -163,7 +163,7 @@ protected:
         const std::string s(t);
         * this << s;
     }
-    // binary files don't include the optional information 
+    // binary files don't include the optional information
     void save_override(
         const boost::archive::class_id_optional_type & /* t */, int
     ){}
@@ -184,13 +184,13 @@ public:
 
     portable_binary_oarchive(
         std::basic_streambuf<
-            std::ostream::char_type, 
+            std::ostream::char_type,
             std::ostream::traits_type
-        > & bsb, 
+        > & bsb,
         unsigned int flags
     ) :
         primitive_base_t(
-            bsb, 
+            bsb,
             0 != (flags & boost::archive::no_codecvt)
         ),
         archive_base_t(flags),
@@ -213,7 +213,7 @@ public:
 /////////1/////////2/////////3/////////4/////////5/////////6/////////7/////////8
 // portable_binary_oarchive.cpp
 
-// (C) Copyright 2002-7 Robert Ramey - http://www.rrsd.com . 
+// (C) Copyright 2002-7 Robert Ramey - http://www.rrsd.com .
 // Use, modification and distribution is subject to the Boost Software
 // License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
@@ -225,7 +225,7 @@ public:
 
 namespace boost { namespace archive {
 
-inline void 
+inline void
 portable_binary_oarchive::save_impl(
     const boost::intmax_t l,
     const char maxsize
@@ -269,7 +269,7 @@ portable_binary_oarchive::save_impl(
     this->primitive_base_t::save_binary(cptr, size);
 }
 
-inline void 
+inline void
 portable_binary_oarchive::init(unsigned int flags) {
     if(m_flags == (endian_big | endian_little)){
         boost::serialization::throw_exception(
@@ -306,7 +306,7 @@ namespace detail {
 
 // template class basic_binary_oprimitive<
 //     portable_binary_oarchive,
-//     std::ostream::char_type, 
+//     std::ostream::char_type,
 //     std::ostream::traits_type
 // > ;
 
