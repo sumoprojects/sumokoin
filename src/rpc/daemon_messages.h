934833922cccfadfbc20e4ee1e42857d172caa80
Sumo Gr <sumogr@mail.com>
2020-06-07 10:24:08 +0300

    [rpc] remove obsolete comments

--- src/rpc/daemon_messages.h
+++ src/rpc/daemon_messages.h
@@ -66,11 +66,7 @@ class classname \
 #define END_RPC_MESSAGE_RESPONSE };
 #define END_RPC_MESSAGE_CLASS };
 
-// NOTE: when using a type with multiple template parameters,
-// replace any comma in the template specifier with the macro
-// above, or the preprocessor will eat the comma in a bad way.
 #define RPC_MESSAGE_MEMBER(type, name) type name = {}
-
 
 namespace cryptonote
 {
