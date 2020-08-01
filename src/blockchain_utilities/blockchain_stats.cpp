1bda5b70bbfd95a4d68903a8bf36e93a63b9d584
Sumo Gr <sumogr@mail.com>
2020-08-01 19:33:45 +0300

    [blockchain-stats] fix underreporting size of prunable txs

--- src/blockchain_utilities/blockchain_stats.cpp
+++ src/blockchain_utilities/blockchain_stats.cpp
@@ -273,6 +273,8 @@ skip:
         return 1;
       }
       currsz += bd.size();
+      if (db->get_prunable_tx_blob(tx_id, bd))
+        currsz += bd.size();      
       currtxs++;
       if (do_hours)
         txhr[currtm.tm_hour]++;
