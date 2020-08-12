21d90cd06870982ebb09c1b08d412f192e21539d
Vu Quang <jimmyvu2008@gmail.com>
2020-08-12 14:43:11 +0700

    [Release] Prepare for release v0.7.0.0
    - Update version to v0.7.0.0 (Yamagata)
    - Add blockchain checkpoint
    - Update checkpoints.dat and hash for it
    - Update README

--- src/cryptonote_core/blockchain.cpp
+++ src/cryptonote_core/blockchain.cpp
@@ -5006,7 +5006,7 @@ void Blockchain::cancel()
 }
 
 #if defined(PER_BLOCK_CHECKPOINT)
-static const char expected_block_hashes_hash[] = "97f3d5f3c1b5a2183234ae83852380cee1443074f7b840c4134bfc948948444f";
+static const char expected_block_hashes_hash[] = "0df1372e29f47c892cec03f346a43d0d38ab541563388887051ba0320e3f9cea";
 void Blockchain::load_compiled_in_block_hashes(const GetCheckpointsCallback& get_checkpoints)
 {
   if (get_checkpoints == nullptr || !m_fast_sync)
