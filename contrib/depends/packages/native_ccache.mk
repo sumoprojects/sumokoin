88ec288f0a6ac29ab5762bda73d29ddad20eddf5
Sumo Gr <sumogr@mail.com>
2020-10-21 22:59:15 +0300

    [depends] update ccache and fix broken link
    it now downloads from its official github repo so we wont have issues with this one any longer

--- contrib/depends/packages/native_ccache.mk
+++ contrib/depends/packages/native_ccache.mk
@@ -1,8 +1,8 @@
 package=native_ccache
-$(package)_version=3.6
-$(package)_download_path=https://www.samba.org/ftp/ccache/
-$(package)_file_name=ccache-$($(package)_version).tar.bz2
-$(package)_sha256_hash=c23ecf1253e0d12c9da9dda9567a88a606d46f93d9982b8b1a423d6f238bd435
+$(package)_version=3.7.12
+$(package)_download_path=https://github.com/ccache/ccache/releases/download/v$($(package)_version)/
+$(package)_file_name=ccache-$($(package)_version).tar.gz
+$(package)_sha256_hash=d2abe88d4c283ce960e233583061127b156ffb027c6da3cf10770fc0c7244194
 
 define $(package)_set_vars
 $(package)_config_opts=
